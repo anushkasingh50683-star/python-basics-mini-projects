f617a82f1d2643ec4229e2365ae8cae778f08a71
Anushka Singh <anushkasingh50683@gmail.com>
2026-01-30 18:51:26 +0530

    Add Rock-Paper-Scissors-Game

---
+++ Rock-Paper-Scissors/main.py
@@ -0,0 +1,27 @@
+import random
+all_moves = ['Rock','Paper','Scissors']
+
+while True:
+    computer_move = random.choice(all_moves)
+    print(computer_move)
+    user_guess = input("Enter your move (rock/paper/scissors): ").lower()
+
+    if computer_move == 'Rock' and user_guess == 'rock':
+        print("Tie")
+    elif computer_move == 'Rock' and user_guess == 'paper':
+        print("You Won!")
+    elif computer_move == 'Rock' and user_guess == 'scissors':
+        print("You Lose!")
+    elif computer_move == 'Paper' and user_guess == 'rock':
+        print("You lose!")
+    elif computer_move == 'Paper' and user_guess == 'paper':
+        print("Tie")
+    elif computer_move == 'Paper' and user_guess == 'scissors':
+        print("You won!")
+    elif computer_move == 'Scissors' and user_guess == 'rock':
+        print("You won!")
+    elif computer_move == 'Scissors' and user_guess == 'paper':
+        print("You Lose!")
+    elif computer_move == 'Scissors' and user_guess == 'scissors':
+        print("Tie")
+
