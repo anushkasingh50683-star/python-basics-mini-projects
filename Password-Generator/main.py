d808285d53c418f22198cfe43f23042491fb62cf
Anushka Singh <anushkasingh50683@gmail.com>
2026-02-01 18:36:40 +0530

    Add string module

--- Password-Generator/main.py
+++ Password-Generator/main.py
@@ -1,21 +1,9 @@
-'''
-import strings
-digits = list(string.digits)
-symbols = list(string.punctuation)
-'''
-
 import random 
+import string
 
-list_of_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
- 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
- 'u', 'v', 'w', 'x', 'y', 'z',
- 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
- 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
- 'U', 'V', 'W', 'X', 'Y', 'Z']
-
-list_of_digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-list_of_symbols = ['!', '#', '$', '%', '&','*', '-', '.' '@']
+list_of_digits = list(string.digits)
+list_of_symbols = list(string.punctuation)
+list_of_letters = list(string.ascii_letters)
 
 no_of_letters = int(input("How many letters you want? "))
 no_of_digits = int(input("How many digits you want? "))
