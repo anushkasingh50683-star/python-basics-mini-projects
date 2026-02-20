409744729518865ddc0f4abeac01908dcbccb9ac
Anushka Singh <anushkasingh50683@gmail.com>
2026-02-20 07:56:17 +0530

    Add sqlite database

---
+++ Student-Management-System/database.py
@@ -0,0 +1,47 @@
+import sqlite3
+
+DB_NAME = "students.db"
+
+def create_table():
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        create table if not exists students(
+            id integer primary key autoincrement,
+            name text not null,
+            marks integer
+                   ) 
+    ''')
+    conn.commit()
+    conn.close()
+
+def insert_data(name,marks):
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        Insert into students (name,marks)Values(?,?)
+    ''',(name,marks,))
+
+    conn.commit()
+    conn.close()
+
+def view_all_data():
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+
+    cursor.execute("Select * from students")
+
+    rows = cursor.fetchall()
+    conn.close()
+     
+    return rows
+   
+'''
+cursor.execute() expects parameters as a sequence (tuple or list).
+And tuples are created using commas.
+The comma is what makes it a tuple.
+When there is only one parameter then , is must otherwise its optional.
+'''
+    
