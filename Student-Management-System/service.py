50fc0409f177913a28ada14c45dd86960d387186
Anushka Singh <anushkasingh50683@gmail.com>
2026-02-23 07:47:38 +0530

    Add update feature in logical layer

--- Student-Management-System/service.py
+++ Student-Management-System/service.py
@@ -1,5 +1,6 @@
 from database import view_all_data
 from database import insert_data
+from database import update_student
 
 def add_student(name,marks):
     name = name.strip()
@@ -21,7 +22,6 @@ def add_student(name,marks):
 
     return "Data entered successfully!"
 
-#update_student_marks()
 #delete_student()
 
 def get_all_students():
@@ -32,3 +32,30 @@ def get_all_students():
     else:
         return rows
     
+def update_student_data(id,new_name,new_marks):
+    try:
+        id = int(id)
+    except ValueError:
+        return "Invalid Id"
+    
+    new_name = new_name.strip()
+
+    if not new_name :
+        return "Error : Name cannot be empty"
+    if len(new_name) < 3:
+        return "Error : Name must be of atleast 3 characters"
+
+    try:
+        new_marks = int(new_marks)
+    except ValueError:
+        return "Invalid marks"
+    
+    if new_marks < 0 or new_marks > 100:
+        return "Error : Enter marks between 0 to 100" 
+    
+    rows = update_student(id,new_name,new_marks)
+
+    if rows == 0:
+        return "Student not found"
+    else:
+        return "Student data updated successfully!"
